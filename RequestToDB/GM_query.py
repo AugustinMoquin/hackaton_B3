437d8c89ef3914ef1032bdc47a862e341e8391e4
aless124 <alessfara28@gmail.com>
2023-10-04 12:52:00 +0200

    Add the API key and the request to get the location

---
+++ RequestToDB/GM_query.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+search_text = str(input())
+
+CityName = "Paris"
+
+url = f'https://api.openrouteservice.org/geocode/search?api_key={API_KEY}&text={search_text},{CityName}'
+response = requests.get(url)
+data = response.json()  
+
+if 'features' in data and data['features']:
+    coordinates = data['features'][0]['geometry']['coordinates']
+    longitude, latitude = coordinates  
+    print(f"Latitude : {latitude}")
+    print(f"Longitude : {longitude}")
+    print(latitude,longitude)
+else:
+    print("Aucune coordonnée trouvée dans la réponse.")
+
+
+headers = {
+    'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+}
+# start = string(input())
+start = f"{longitude},{latitude}"  # Coordonnées de base
+end = f"{longitude},{latitude}" # Coordonnées de fin
+call = requests.get(f'https://api.openrouteservice.org/v2/directions/cycling-road?api_key={API_KEY}&start='+start+'&end='+end, headers=headers)
+
+#print(call.status_code, call.reason)
+#print(call.text)
+
+#call = requests.get(f'https://api.openrouteservice.org/geocode/search?api_key={API_KEY}&text=kirane\'s', headers=headers)
+print(call.status_code, call.reason)
+print(call.text)
+
