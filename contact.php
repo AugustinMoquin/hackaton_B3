5f694fac7bca7f2cab4eeac9e9aa9957639cd79e
S0nia11 <146825870+S0nia11@users.noreply.github.com>
2023-10-06 23:15:43 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width", initial-scale="1.0">
+  <title>BYKERIDE Contact</title>
+  <link rel="stylesheet" type="text/css" href="contact.css">
+</head>
+<body>
+  
+  <div class="wrapper">
+      <form action="">
+         <h2>Contact</h2>
+         <div class="input-box">
+              <div class="Facebook-link">
+               <a href="#">Facebook</a>
+      
+         <div class="input-box">
+              <div class="Instagram-link">
+               <a href="#">Instagram</a>
+              </div>
+          <div class="input-box">
+           <div class="Gmail-Link">
+               <a href="#">Gmail</a>
+           </div>
+	
+         </div>
+   
+   </div>
+	
+      </form>
+  </div>
+  
+</body>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300,400,500,600,700,800,900&display=swap");
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+
+}
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: url(B.jpg) no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+.wrapper {
+    widhth: 420px;
+    background: transparent;
+    border: 2px solid, rgba(255, 255, 255, .2);
+    backdrop-filter: blur(20px);
+    box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+    color: black;
+    border-radius: 10px;
+
+}
+.wrapper h2 {
+    font-size: 36px;
+    text-align: center;
+    color:black;
+    margin-top: 25px;
+
+}
+
+.wrapper .input-box {
+    width: 350px;
+    height: 300px;
+    margin: 30px 0;
+    border-radius: 40px;
+}
+.Facebook-link {
+    position: relative;
+    text-align: center;
+    margin-top: 100px;
+    font-size: 30px;
+   
+}
+.Facebook-link :hover{
+    text-decoration: underline;
+ 
+}
+
+</style>
+</html>
